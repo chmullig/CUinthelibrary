f52fe35cc66d503ad09dd21ec1dd71806e1e6b96
Chris Mulligan <clm2186@columbia.edu>
2013-03-06 11:30:45 -0500

    Added GPS coordinates. Another SQL query for open/closed status.

--- opennow.sql
+++ opennow.sql
@@ -1,1 +1,5 @@
+--List those libraries open right now.
 select * from hours where datetime("now", "localtime") between datetime(open) and datetime(close);
+
+--For every library that has a record for today, list whether it's open or closed right now
+select *, case when datetime("now", "localtime") between datetime(open) and datetime(close) then 'OPEN' else 'CLOSED' end as open from hours where date(date) = date();
