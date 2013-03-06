585cadc2fb1e2cbbb48d068b4ebd1d2a6bdcf10a
Chris Mulligan <clm2186@columbia.edu>
2013-03-05 23:09:06 -0500

    Fetches and parses dates and times correctly.

--- fetch.py
+++ fetch.py
@@ -1,11 +1,11 @@
 import json
 import csv
-import datetime
+from datetime import datetime, date, time, timedelta
 
 from bs4 import BeautifulSoup
 import requests
 
-now = datetime.date.today()
+now = date.today()
 
 params = {"function" : "view", "edit_mode" : "individual", "mode" : "month",
     "library" : None, "year" : now.year, "day" : now.timetuple().tm_yday}
@@ -14,7 +14,22 @@ baseurl = "http://hours.library.columbia.edu"
 
 libraries = csv.DictReader(open("libraries.txt"), delimiter="\t")
 
+def preptime(dt, timeStr):
+    if timeStr == "Noon":
+        parsedTime = time(12,00)
+        dttime = datetime.combine(dt, parsedTime)
+    elif timeStr == "Midnight":
+        parsedTime = time(0,00)
+        dttime = datetime.combine(dt, parsedTime)+timedelta(days=1)
+    else:
+        parsedTime = datetime.strptime(timeStr, "%I:%M%p").time()
+        dttime = datetime.combine(dt, parsedTime)
+    return dttime
+
+
 for library in libraries:
+    if library["library"].startswith("#"):
+        continue
     params["library"] = library["library"]
     r = requests.get(baseurl, params=params)
     s = BeautifulSoup(r.text)
@@ -22,3 +37,36 @@ for library in libraries:
         library["library"], library["name"],
         s.find("big").find(class_="gray").text.strip(": "),
         s.find("big").find(class_="color").text)
+
+    days = sorted(s.find_all(class_='date') + s.find_all(class_='today_date'))
+    for day in days:
+        notes = day.parent.parent.find(class_="day_notes").text.strip()
+        dt = date(now.year, now.month, int(day.text))
+        try:
+            hours = day.parent.parent.find(class_="today_hours").text.strip()
+            if hours.lower() == "24 hours":
+                opens = datetime.combine(dt, time(0, 0))
+                closes = opens+timedelta(days=1)
+            else:
+                openStr, closeStr = hours.split("-")
+                opens = preptime(dt, openStr)
+                closes = preptime(dt, closeStr)
+                if closes.time() < time(8, 00):
+                    #it closes after midnight
+                    closes = closes+timedelta(days=1)
+            status = ""
+        except AttributeError:
+            status = day.parent.parent.find("b").text.strip()
+            opens = None
+            closes = None
+            
+
+        msg = "\t\t%s: %s to %s" % (dt, opens, closes)
+        try:
+            if status:
+                msg += " " + status
+        except NameError:
+            pass
+        if notes:
+            msg += " (%s)" % notes
+        print msg
