42786248741ad096ff28454776f632877011199b
Chris Mulligan <clm2186@columbia.edu>
2013-03-30 17:17:42 -0400

    Tweaked query... show which libraries are open, sorted by how much longer they're open for...

--- opennow.sql
+++ opennow.sql
@@ -1,5 +1,6 @@
 --List those libraries open right now.
-select * from hours where datetime("now", "localtime") between datetime(open) and datetime(close);
+select *, round((julianday(close)-julianday("now", "localtime"))*24, 2) from hours where datetime("now", "localtime") between datetime(open) and datetime(close)
+order by round((julianday(close)-julianday("now", "localtime"))*24, 2) ;
 
 --For every library that has a record for today, list whether it's open or closed right now
-select *, case when datetime("now", "localtime") between datetime(open) and datetime(close) then 'OPEN' else 'CLOSED' end as open from hours where date(date) = date();
+--select *, case when datetime("now", "localtime") between datetime(open) and datetime(close) then 'OPEN' else 'CLOSED' end as open from hours where date(date) = date();
